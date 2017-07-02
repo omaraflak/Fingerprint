5eff68ce50172e609cefface9082b8ba20287824
omaflak <aflakomar@hotmail.fr>
2017-07-02 20:05:08 +0200

    added button

--- app/src/main/java/me/aflak/fingerprintdialoglibrary/MainActivity.java
+++ app/src/main/java/me/aflak/fingerprintdialoglibrary/MainActivity.java
@@ -2,28 +2,31 @@ package me.aflak.fingerprintdialoglibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 import me.aflak.libraries.FingerprintDialog;
 import me.aflak.libraries.FingerprintCallback;
 
 public class MainActivity extends AppCompatActivity implements FingerprintCallback {
-    private TextView textView;
+    private FingerprintDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text);
+        dialog = new FingerprintDialog(this);
 
-        FingerprintDialog dialog = new FingerprintDialog(this);
-        dialog.show("Sign In", "Confirm fingerprint to continue", this);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show("Sign In", "Confirm fingerprint to continue", MainActivity.this);
+            }
+        });
     }
 
     @Override
     public void onFingerprintSuccess() {
-        textView.setText("Welcome !");
     }
 
     @Override
